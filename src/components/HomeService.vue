40cb0dc6318102d99e34688edf85df9697475996
Humberto Rondón <git@achequisde.com>
2026-02-14 12:03:56 -0400

    Add styling to the HomeService component

--- src/components/HomeService.vue
+++ src/components/HomeService.vue
@@ -7,8 +7,38 @@ const props = defineProps<{
 
 <template>
   <article>
-    <h3>{{ props.title }}</h3>
+    <h3 class="title">{{ props.title }}</h3>
     <p>{{ props.text }}</p>
     <button>Leer más >></button>
   </article>
 </template>
+
+<style scoped>
+article {
+  padding: 1rem;
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  border: 1px solid black;
+}
+
+article>* {
+  padding: 0.5em;
+}
+
+button {
+  border: 0px;
+  background: inherit;
+
+  font-size: 1.0325rem;
+
+  font-weight: bold;
+}
+
+h3 {
+  font-size: 1.25rem;
+}
+</style>

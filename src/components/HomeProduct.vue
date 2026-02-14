52ff610002cbc2fad477096426c50fc9957dc99d
Humberto Rondón <git@achequisde.com>
2026-02-14 11:07:35 -0400

    Add props to HomeProduct

--- src/components/HomeProduct.vue
+++ src/components/HomeProduct.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  src: string
+  alt: string
+  title: string
+}>();
+</script>
+
 <template>
-  <h1></h1>
+  <img :src="props.src" :alt="props.alt">
+  <h3>{{ props.title }}</h3>
 </template>

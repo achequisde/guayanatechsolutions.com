4652b380cbce6abcd5a6409c21fe8e0a98fd3e1f
Humberto Rondón <git@achequisde.com>
2026-02-14 09:30:26 -0400

    Add titles to Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts">
+
+const titles = [
+  "innovation, solutions, and efficiency",
+  "our services",
+  "our products",
+].map((e) => e.toUpperCase());
+
 </script>
 
 <template>
   <main>
+    <section>
+      <h1>{{ titles[0] }}</h1>
+    </section>
+
+    <section>
+
+    </section>
+
+    <section>
+      <h2>{{ titles[1] }}</h2>
+    </section>
+
+    <section>
+
+    </section>
+
+    <section>
+      <h2>{{ titles[2] }}</h2>
+    </section>
+
+    <section>
+    </section>
   </main>
 </template>

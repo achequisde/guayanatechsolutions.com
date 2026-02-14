9ce2ddb0de0904e1418925f176ebe168c490bb54
Humberto Rondón <git@achequisde.com>
2026-02-14 09:42:55 -0400

    Add welcome message section content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,8 +14,16 @@ const titles = [
       <h1>{{ titles[0] }}</h1>
     </section>
 
-    <section>
-
+    <section class="welcome-message">
+      <div></div>
+      <div>
+        <h2>Guayana Tech Solutions</h2>
+        <p>
+          Somos una empresa especializada en Ingeniería Electrónica Industrial, con una amplia trayectoria en el área,
+          te invitamos a conocer más sobre nosotros y lo que tenemos para ti.
+        </p>
+        <button class="btn btn--primary">Read more</button>
+      </div>
     </section>
 
     <section>
@@ -34,3 +42,29 @@ const titles = [
     </section>
   </main>
 </template>
+
+<style>
+.welcome-message {
+  display: grid;
+
+  grid-template-columns: 1fr 1fr;
+}
+
+.welcome-message>* {
+  width: 50%;
+}
+
+.btn {
+  padding: 10px 15px;
+  font-weight: bold;
+
+  border: 0px;
+
+  border-radius: 25px;
+}
+
+.btn--primary {
+  color: white;
+  background: #FF8300;
+}
+</style>
